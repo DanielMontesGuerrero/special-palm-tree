64cc91e661db1ae42a1b90208d215e6d96a61e25
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2022-02-05 16:20:28 -0600

    Implementación de la clase arrow

---
+++ test/Arrow.test.ts
@@ -0,0 +1,15 @@
+import Arrow from '../src/models/Arrow';
+import Vector2 from '../src/models/Vector2';
+
+describe('Arrow', () => {
+  test('move', () => {
+    const arrow = new Arrow(0, Math.PI);
+    arrow.move(1);
+    expect(arrow.angleToVector2()).toBeVector2(new Vector2(-1, 0));
+
+    arrow.move(1.25);
+    expect(arrow.angleToVector2()).toBeVector2(
+      new Vector2(Math.cos(Math.PI / 4), Math.sin(Math.PI / 4)),
+    );
+  });
+});
